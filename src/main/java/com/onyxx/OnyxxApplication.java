d87e0219365d418ec15d21bb5d38139cf32e9989
azamk <azam.khan@tijoree.money>
2024-09-10 13:20:10 +0530

    Initial commit

---
+++ src/main/java/com/onyxx/OnyxxApplication.java
@@ -0,0 +1,13 @@
+package com.onyxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnyxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnyxxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onyxx/OnyxxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onyxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnyxxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
